2767510851c379a2287a4490229a528507f41377
I Made Cakra <imadecakra20@gmail.com>
2023-11-25 11:40:25 +0800

    Membuat CRUD Article

---
+++ routes/posts.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const Post = require('../models/Post');
+
+// GET ALL POST
+router.get('/', async (req, res) => {
+    try {
+        const post = await Post.find().sort({ createdAt: 'desc' })
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).json({ "message" : "Post tidak ditemukan"})
+    }
+})
+
+// GET POST by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) res.json({ "message" : "Post tidak ditemukan"})
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+})
+
+//CREATE POST
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+      const savedPost = await newPost.save();
+      res.status(201).json(savedPost);
+    } catch (err) {
+      res.status(500).json({ "message" : "Gagal Membuat Post"});
+    }
+});
+
+// DELETE POST
+router.delete('/:id', async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ "message" : "Berhasil menghapus post"});
+    } catch (error) {
+        res.status(500).json({ "message" : "Gagal menghapus post"});
+    }
+})
+
+// EDIT POST
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id,{
+            $set: req.body
+        }, {new: true});
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(500).json({ "message" : "Gagal Mengedit post"});
+    }
+})
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-
-
-mongoose.connect(`${process.env.MONGO_URL}`).then(console.log('connected to mongodb')).catch( err => console.log(err));
-
+const postRouter = require('./routes/posts'); 
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.use(express.json())
 
+mongoose.connect(`${process.env.MONGO_URL}`).then(console.log('connected to mongodb')).catch( err => console.log(err));
+
+
+app.use('/api/articles', postRouter)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
