3d527ff1cf11017c3bf565f2be0f8c63eac816bf
I Made Cakra <imadecakra20@gmail.com>
2023-11-28 19:01:51 +0800

    make server.js more readable

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
-// eslint-disable-next-line no-unused-vars
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
-const postRouter = require('./routes/posts');
-const catRouter = require('./routes/categories');
 
-const PORT = process.env.PORT || 5000;
 const app = express();
 
+// access environment variable
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config();
+
+// connect to database
+require('./config/database');
+
+// middleware
 app.use(express.json());
 
-mongoose.connect(`${process.env.MONGO_URL}`).then(console.log('connected to mongodb')).catch((err) => console.log(err));
+// routes
+const postRouter = require('./routes/posts');
 
+// call port from env
+const PORT = process.env.PORT || 5000;
+
+// routes middleware
 app.use('/api/articles', postRouter);
-app.use('/api/categories', catRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
