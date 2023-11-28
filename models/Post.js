6aad35a223a1b8529fb087308dc3a2a786151b74
I Made Cakra <imadecakra20@gmail.com>
2023-11-28 10:48:48 +0800

    menambahkan endpoint category & query parameter pada post

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    thumb: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true 
-    },
-    desc: {
-        type: String,
-        required: true
-    },
-    categories: {
-        type: Array,
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  thumb: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: Array,
+  },
 
-}, { timestamps:true })
+}, { timestamps: true });
 
-module.exports = mongoose.model('Post', PostSchema)
+module.exports = mongoose.model('Post', PostSchema);
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
+// eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-const postRouter = require('./routes/posts'); 
-const PORT = process.env.PORT || 5000
+const postRouter = require('./routes/posts');
+const catRouter = require('./routes/categories');
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect(`${process.env.MONGO_URL}`).then(console.log('connected to mongodb')).catch( err => console.log(err));
+mongoose.connect(`${process.env.MONGO_URL}`).then(console.log('connected to mongodb')).catch((err) => console.log(err));
 
-
-app.use('/api/articles', postRouter)
+app.use('/api/articles', postRouter);
+app.use('/api/categories', catRouter);
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});
