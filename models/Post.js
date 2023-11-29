3b63f8f1296fcd02bb0d7902ccb45c8836e46630
I Made Cakra <imadecakra20@gmail.com>
2023-11-29 10:53:53 +0800

    update schema

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,10 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  content: {
+    type: String,
+    required: true,
+  },
   categories: {
     type: Array,
   },
